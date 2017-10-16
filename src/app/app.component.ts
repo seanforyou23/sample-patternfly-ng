6c88c1db14720ddd68a6bfd9e30b1326e85f845c
Michael Spaxman <seanforyou23@gmail.com>
2017-10-16 13:42:51 -0400

    initial notes, hook in patternfly for examples

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // styles here are scoped to the markup template above
   styleUrls: ['./app.component.css']
 })
+
+// Make this component available to the rest of the the application
+// and define variables and functions that are made available to the component template
 export class AppComponent {
-  title = 'app';
+  // title will be available as {{title}} in the view template
+  title = 'First NG App';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,11 @@
+// standard for ng apps
 import { BrowserModule } from '@angular/platform-browser';
+// decorator allowing you to place all imports, declarations, and bootstrap files
+// for a particular module in a single location
 import { NgModule } from '@angular/core';
-
+// custom module to use in the app
+import { NotificationModule } from 'patternfly-ng';
+// the root app component
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +13,12 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NotificationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
+// referenced from main.ts
 export class AppModule { }
